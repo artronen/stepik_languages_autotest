dfcabc9cab0986c429812e4d8757562836a22024
Arthur Mironienko <art.ronen1@gmail.com>
2021-10-31 19:58:39 +0300

    update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 def test_localization_site(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    message_fr = "Ajouter au panier"
+    messages = ["Ajouter au panier", "Añadir al carrito", 'Добавить в корзину', 'Add to basket']
     browser.get(link)
     button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
     assertion_text = button.text
-    assert message_fr == assertion_text
+    assert assertion_text in messages
